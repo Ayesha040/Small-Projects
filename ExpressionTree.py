a4892ab275755424e1781438fdcf1d45b59399e7
ayesha faheem <46402398+Ayesha040@users.noreply.github.com>
2023-03-09 16:01:16 -0600

    Add files via upload

---
+++ ExpressionTree.py
@@ -0,0 +1,88 @@
+#  File: ExpressionTree.py
+
+#  Description:
+
+#  Student Name:
+
+#  Student UT EID:
+
+#  Partner Name:
+
+#  Partner UT EID:
+
+#  Course Name: CS 313E
+
+#  Unique Number: 
+
+#  Date Created:
+
+#  Date Last Modified:
+
+import sys
+
+operators = ['+', '-', '*', '/', '//', '%', '**']
+
+class Stack (object):
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append (data)
+
+    def pop(self):
+        if(not self.is_empty()):
+            return self.stack.pop()
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+class Node (object):
+    def __init__ (self, data = None, lChild = None, rChild = None):
+        self.data = data
+        self.lChild = lChild
+        self.rChild = rChild
+
+class Tree (object):
+    def __init__ (self):
+        self.root = None
+    
+    # this function takes in the input string expr and 
+    # creates the expression tree
+    def create_tree (self, expr):
+    
+    # this function should evaluate the tree's expression
+    # returns the value of the expression after being calculated
+    def evaluate (self, aNode):
+    
+    # this function should generate the preorder notation of 
+    # the tree's expression
+    # returns a string of the expression written in preorder notation
+    def pre_order (self, aNode):
+
+    # this function should generate the postorder notation of 
+    # the tree's expression
+    # returns a string of the expression written in postorder notation
+    def post_order (self, aNode):
+
+# you should NOT need to touch main, everything should be handled for you
+def main():
+    # read infix expression
+    line = sys.stdin.readline()
+    expr = line.strip()
+ 
+    tree = Tree()
+    tree.create_tree(expr)
+    
+    # evaluate the expression and print the result
+    print(expr, "=", str(tree.evaluate(tree.root)))
+
+    # get the prefix version of the expression and print
+    print("Prefix Expression:", tree.pre_order(tree.root).strip())
+
+    # get the postfix version of the expression and print
+    print("Postfix Expression:", tree.post_order(tree.root).strip())
+
+if __name__ == "__main__":
+    main()
